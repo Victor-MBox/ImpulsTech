27e2b3884a17c83a9f6347063029c45a9d914e90
Victor Matsakov <victor.matsakov7@gmail.com>
2023-11-14 13:52:33 +0600

    14.11

--- js/script.js
+++ js/script.js
@@ -22,40 +22,49 @@ document.addEventListener('click', function (event) {
 	}
 })
 
-
 // * Поиск
 
 // Обработчик клика для поиска
-document.getElementById('searchBtn').addEventListener('click', function (event) {
-	this.classList.toggle('search_active')
-	document.getElementById('searchWindow').classList.toggle('search-window_active')
-    document.body.classList.toggle('no-scroll')
-})
+document
+	.getElementById('searchBtn')
+	.addEventListener('click', function (event) {
+		this.classList.toggle('search_active')
+		document
+			.getElementById('searchWindow')
+			.classList.toggle('search-window_active')
+		document.body.classList.toggle('no-scroll')
+	})
 
 // Функция для удаления классов
 function removeActiveClasses() {
-    document.getElementById('searchBtn').classList.remove('search_active');
-    document.getElementById('searchWindow').classList.remove('search-window_active');
-    document.body.classList.remove('no-scroll');
+	document.getElementById('searchBtn').classList.remove('search_active')
+	document
+		.getElementById('searchWindow')
+		.classList.remove('search-window_active')
+	document.body.classList.remove('no-scroll')
 }
 
 // Обработчик для кнопки закрытия
-document.querySelector('.search-window__close').addEventListener('click', removeActiveClasses);
+document
+	.querySelector('.search-window__close')
+	.addEventListener('click', removeActiveClasses)
 
 // Обработчик для клика вне формы поиска и кнопки закрытия внутри .search-window
-document.getElementById('searchWindow').addEventListener('click', function(event) {
-    if (!event.target.closest('.search-window__wrapper')) {
-        removeActiveClasses();
-    }
-});
+document
+	.getElementById('searchWindow')
+	.addEventListener('click', function (event) {
+		if (!event.target.closest('.search-window__wrapper')) {
+			removeActiveClasses()
+		}
+	})
 
 // Обработчик для закрытия по клавише Esc
-document.addEventListener('keydown', function(event) {
-    if (event.keyCode === 27) { // 27 - код клавиши Esc
-        removeActiveClasses();
-    }
-});
-
+document.addEventListener('keydown', function (event) {
+	if (event.keyCode === 27) {
+		// 27 - код клавиши Esc
+		removeActiveClasses()
+	}
+})
 
 /* //! Модальные окна */
 /* //* Главное окно */
@@ -64,18 +73,18 @@ const modalWrapper = document.querySelector('.modal')
 const modalMain = document.querySelector('.modal__main')
 
 const bntMainModal = document.querySelector('[data-modal="mainModal"]')
-const bntCloseMainModal = document.querySelector('[data-modal="closeMainModal"]')
-
-
+const bntCloseMainModal = document.querySelector(
+	'[data-modal="closeMainModal"]'
+)
 
 // Обработчик клика для модального окна
 bntMainModal.addEventListener('click', addActiveClassesModal)
 
 // Функция для добавления классов
 function addActiveClassesModal() {
-    modalWrapper.classList.add('active-modal')
-    modalMain.classList.add('active-modal')
-    document.body.classList.toggle('no-scroll')
+	modalWrapper.classList.add('active-modal')
+	modalMain.classList.add('active-modal')
+	document.body.classList.toggle('no-scroll')
 }
 
 // Обработчик для кнопки закрытия
@@ -83,9 +92,9 @@ bntCloseMainModal.addEventListener('click', removeActiveClassesModal)
 
 // Функция для удаления классов
 function removeActiveClassesModal() {
-    modalWrapper.classList.remove('active-modal')
-    modalMain.classList.remove('active-modal')
-    document.body.classList.remove('no-scroll');
+	modalWrapper.classList.remove('active-modal')
+	modalMain.classList.remove('active-modal')
+	document.body.classList.remove('no-scroll')
 }
 
 // Обработчик для клика по оверлею
@@ -96,12 +105,12 @@ modalWrapper.addEventListener('click', function (event) {
 })
 
 // Обработчик для закрытия по клавише Esc
-document.addEventListener('keydown', function(event) {
-    if (event.keyCode === 27) { // 27 - код клавиши Esc
-        removeActiveClassesModal()
-    }
-});
-
+document.addEventListener('keydown', function (event) {
+	if (event.keyCode === 27) {
+		// 27 - код клавиши Esc
+		removeActiveClassesModal()
+	}
+})
 
 /* //! Слайдер на главной странице */
 
@@ -131,3 +140,39 @@ $(document).ready(function () {
 		slickSlider.slick('slickNext')
 	})
 })
+
+/* //* Таб на главной */
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Открытие первой вкладки по умолчанию
+	openTab(null, 'Tab1')
+
+	// Добавляем класс активности первой кнопке
+	var firstTabButton = document.querySelector('.btn-tab')
+	if (firstTabButton) {
+		firstTabButton.classList.add('btn-tab_active')
+	}
+})
+
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks
+
+	// Скрываем весь контент вкладок
+	tabcontent = document.getElementsByClassName('article')
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none'
+	}
+
+	// Убираем активность со всех вкладок
+	tablinks = document.getElementsByClassName('btn-tab')
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].classList.remove('btn-tab_active')
+	}
+
+	// Отображаем текущий контент вкладки и делаем вкладку активной
+	document.getElementById(tabName).style.display = 'flex'
+	if (evt) {
+		evt.currentTarget.classList.add('btn-tab_active')
+	}
+}
+
