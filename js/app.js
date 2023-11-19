e4d29c0e96c7147390039a945e6afe6a4adff342
Victor Matsakov <victor.matsakov7@gmail.com>
2023-11-19 12:47:07 +0600

    19.11

--- js/app.js
+++ js/app.js
@@ -19,3 +19,34 @@ $(document).ready(function () {
 		}
 	}
 }) 
+
+
+// * Слайдер с отзывами
+if (window.jQuery) {
+	$(document).ready(function () {
+		let sliderReviews = $('#sliderReviews')
+
+		if (sliderReviews.length) {
+			sliderReviews.slick({
+				// Ваши настройки для Slick Slider
+				infinite: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				dots: true,
+				speed: 500,
+				fade: false,
+				autoplay: true,
+				autoplaySpeed: 7000,
+			})
+
+			$('#sliderReviewsPrev').on('click', function () {
+				sliderReviews.slick('slickPrev')
+			})
+
+			$('#sliderReviewsNext').on('click', function () {
+				sliderReviews.slick('slickNext')
+			})
+		}
+	})
+}
